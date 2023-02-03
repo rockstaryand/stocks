23c74091ea3f4e176cf1cf74daac6ff4e488411a
Yandisa <yandi@LAPTOP-74A1OF39>
2023-02-03 15:15:51 +0200

    "Initial commit"

---
+++ Controllers/StocksController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Text.Json;
+namespace StockApi.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    
+   
+        public class ValuesController : ControllerBase
+        {
+            [HttpGet("Stocks")]
+            public IActionResult GetFile1Data()
+            {
+                string content = System.IO.File.ReadAllText("Data/Stocks.json");
+                var stock = JsonSerializer.Deserialize<object>(content);
+
+                return Ok(stock);
+            }
+
+            [HttpGet("StockValues")]
+            public IActionResult GetFile2Data()
+            {
+                string content = System.IO.File.ReadAllText("Data/StockValues.json");
+                var stock = JsonSerializer.Deserialize<object>(content);
+
+                return Ok(stock);
+            }
+    }
+    
+}
---
+++ Models/Stocks.cs
@@ -0,0 +1,11 @@
+﻿namespace StockApi.Models
+{
+    public class Stocks
+    {
+        public int id { get; set; }
+        public string stock { get; set; }
+        public string industry { get; set; }
+        public string sector { get; set; }
+        public string currency_code { get; set; }
+    }
+}
